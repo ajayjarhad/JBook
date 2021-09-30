5f04b9b4ed626505bb127b923e8fe7aa0deb2988
Ajay Jarhad <ajayjarhad1@gmail.com>
2021-10-01 01:44:44 +0530

    refactor: Implemented redux

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,40 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-import bundle from "../bundler";
 import Resizable from "./resizable";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
+import { useActions } from "../hooks/use-actions";
+import { Cell } from "../state";
+import { useTypedSelector } from "../hooks/use-typed-selector";
 
-const CodeCell = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-  const [err, setErr] = useState("");
+import "./code-cell.css";
+interface CodeCellProps {
+  cell: Cell;
+}
 
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector(
+    (state) => state.bundles && state.bundles[cell.id]
+  );
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
-      setCode(output.code);
-      setErr(output.err);
-    }, 1000);
+      createBundle(cell.id, cell.content);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.content, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          height: "calc(100% - 10px)",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 5;"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {/* <div>
-          <button onClick={onClick}>Submit</button>
-        </div> */}
-        <Preview code={code} bundlingStatus={err} />
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
