527583c15852a352f69a06788c10a31980b500e6
Ajay Jarhad <ajayjarhad1@gmail.com>
2021-10-04 00:28:34 +0530

    Pushing to NPM

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,10 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var serve = function (port, filename, dir) {
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var path_1 = __importDefault(require("path"));
+var cell_1 = require("./routes/cell");
+var serve = function (port, filename, dir, useProxy) {
     var app = (0, express_1.default)();
-    app.listen(port, function () {
-        console.log("listening on port ", port);
+    app.use((0, cell_1.createCellsRouter)(filename, dir));
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: "http://localhost:3000",
+            ws: true,
+            logLevel: "silent",
+        }));
+    }
+    else {
+        var packagePath = require.resolve("@js-workbook/client/build/index.html");
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
+    return new Promise(function (resolve, reject) {
+        app.listen(port, resolve).on("error", reject);
     });
 };
 exports.serve = serve;
