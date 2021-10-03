71d9925011ec89eca2b5220aa993561ea34ab8d9
Ajay Jarhad <ajayjarhad1@gmail.com>
2021-10-04 00:27:51 +0530

    Pushing to NPM

---
+++ packages/cli/dist/notebook.js
@@ -0,0 +1,1 @@
+[{"content":"show(<h1>Hello</h1>)","type":"code","id":"hgy"}]
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,14 +1,32 @@
 import path from "path";
 import { Command } from "commander";
-import { on } from "events";
-import { serve } from "local-api";
+import { serve } from "@js-workbook/api";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Opens a file for editing")
   .option("-p, --port <number>", "Port to run the server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+      console.log(
+        `File ${filename} is opened. Navigate to  http://localhost:${options.port} to edit the file `
+      );
+    } catch (err: string | any) {
+      if (err.code === "EADDRINUSE")
+        console.error(
+          `The port ${options.port} is in use, please select a different port `
+        );
+      else console.log("Error occured ", err.message);
 
-    serve(parseInt(options.port), path.basename(filename), dir);
+      process.exit(1);
+    }
   });
